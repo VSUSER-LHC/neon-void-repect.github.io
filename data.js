ed6ed71e875d42c4b3b7cd1a41909452cfff07d5
VSUSER-LHC <hiroki.k.2026@gmail.com>
2026-05-18 13:28:46 +0900

    Refactor SHIPS and ENEMIES data structures

--- data.js
+++ data.js
@@ -1,21 +1,13 @@
-const SHIPS = {
-    fighter: {
-        speed: 4,
-        hp: 100
-    }
+export const SHIPS = {
+  fighter: {
+    speed: 220,
+    fireRate: 250
+  }
 };
 
-const ENEMIES = {
-    normal: {
-        speed: 1.2,
-        hp: 50
-    },
-    tank: {
-        speed: 0.6,
-        hp: 200
-    },
-    fast: {
-        speed: 2.5,
-        hp: 30
-    }
+export const ENEMIES = {
+  basic: {
+    hp: 3,
+    fireRate: 1200
+  }
 };
