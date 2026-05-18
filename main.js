d3055e21fc83f3bd18a3b4ec427d06ee7fdc16ab
VSUSER-LHC <hiroki.k.2026@gmail.com>
2026-05-18 12:53:11 +0900

    Set up Phaser game configuration and main functions
    Initialize Phaser game with player and enemy setup.

---
+++ main.js
@@ -0,0 +1,83 @@
+import { Player } from "./player.js";
+import { Enemy } from "./enemy.js";
+import { STAGES } from "./stage.js";
+import { SHIPS, ENEMIES } from "./data.js";
+
+let player;
+let enemies = [];
+let cursors;
+let bullets;
+let enemyBullets;
+
+const config = {
+    type: Phaser.AUTO,
+    width: 960,
+    height: 540,
+    backgroundColor: "#000000",
+
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
+        }
+    },
+
+    scene: {
+        preload,
+        create,
+        update
+    }
+};
+
+new Phaser.Game(config);
+
+function preload() {
+
+}
+
+function create() {
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    bullets = this.physics.add.group();
+    enemyBullets = this.physics.add.group();
+
+    player = new Player(
+        this,
+        480,
+        450,
+        SHIPS.fighter
+    );
+
+    loadStage(this, 1);
+}
+
+function update(time, delta) {
+
+    player.update(cursors, bullets, time);
+
+    enemies.forEach(enemy => {
+        enemy.update(player, enemyBullets, time);
+    });
+}
+
+function loadStage(scene, stageNumber) {
+
+    enemies = [];
+
+    const stage = STAGES[stageNumber];
+
+    stage.enemies.forEach(data => {
+
+        const enemyData = ENEMIES[data.type];
+
+        const enemy = new Enemy(
+            scene,
+            data.x,
+            data.y,
+            enemyData
+        );
+
+        enemies.push(enemy);
+    });
+}
