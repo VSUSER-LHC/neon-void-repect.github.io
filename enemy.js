fe7a8185f9b5e6594bb84bf39a7dbfd2be1781df
VSUSER-LHC <hiroki.k.2026@gmail.com>
2026-05-18 13:28:10 +0900

    Refactor Enemy class to use ES6 module syntax

--- enemy.js
+++ enemy.js
@@ -1,19 +1,37 @@
-class Enemy {
-    constructor(scene, x, y, data) {
-        this.scene = scene;
+export class Enemy {
+  constructor(scene, x, y, data) {
+    this.scene = scene;
+    this.hp = data.hp;
+    this.fireRate = data.fireRate;
+    this.lastShot = 0;
 
-        this.sprite = scene.add.rectangle(x, y, 25, 25, 0xff0000);
-        scene.physics.add.existing(this.sprite);
+    this.sprite = scene.physics.add.rectangle(x, y, 28, 28, 0xff0000);
+  }
 
-        this.speed = data.speed;
-        this.hp = data.hp;
+  update(player, enemyBullets, time) {
+    // ちょい追従
+    if (player) {
+      const dx = player.sprite.x - this.sprite.x;
+      this.sprite.setVelocityX(dx * 0.01);
     }
 
-    update(player) {
-        this.scene.physics.moveToObject(
-            this.sprite,
-            player.sprite,
-            this.speed * 50
-        );
+    // 射撃
+    if (time > this.lastShot) {
+      this.shoot(enemyBullets);
+      this.lastShot = time + this.fireRate;
     }
+  }
+
+  shoot(group) {
+    const b = this.scene.physics.add.rectangle(
+      this.sprite.x,
+      this.sprite.y + 20,
+      5,
+      10,
+      0xff8800
+    );
+
+    b.body.setVelocityY(200);
+    group.add(b);
+  }
 }
