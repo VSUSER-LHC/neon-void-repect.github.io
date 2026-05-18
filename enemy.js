28e0c84a1625781380d25a1124390388d8bf8b42
VSUSER-LHC <hiroki.k.2026@gmail.com>
2026-05-18 16:22:44 +0900

    Refactor enemy sprite handling in enemy.js

--- enemy.js
+++ enemy.js
@@ -5,17 +5,17 @@ export class Enemy {
     this.fireRate = data.fireRate;
     this.lastShot = 0;
 
-    this.sprite = scene.physics.add.rectangle(x, y, 28, 28, 0xff0000);
+    this.sprite = scene.physics.add.sprite(x, y, null);
+    this.sprite.setDisplaySize(28, 28);
+    this.sprite.setTint(0xff0000);
   }
 
   update(player, enemyBullets, time) {
-    // ちょい追従
     if (player) {
       const dx = player.sprite.x - this.sprite.x;
       this.sprite.setVelocityX(dx * 0.01);
     }
 
-    // 射撃
     if (time > this.lastShot) {
       this.shoot(enemyBullets);
       this.lastShot = time + this.fireRate;
@@ -23,15 +23,16 @@ export class Enemy {
   }
 
   shoot(group) {
-    const b = this.scene.physics.add.rectangle(
+    const b = this.scene.physics.add.sprite(
       this.sprite.x,
       this.sprite.y + 20,
-      5,
-      10,
-      0xff8800
+      null
     );
 
+    b.setDisplaySize(5, 10);
+    b.setTint(0xff8800);
     b.body.setVelocityY(200);
+
     group.add(b);
   }
 }
