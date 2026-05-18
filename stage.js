c882db034ad44211692b5fc3542ee878664f4048
VSUSER-LHC <hiroki.k.2026@gmail.com>
2026-05-18 12:55:06 +0900

    Add STAGES constant with enemy configurations

---
+++ stage.js
@@ -0,0 +1,44 @@
+export const STAGES = {
+
+    1: {
+
+        enemies: [
+
+            {
+                type: "basic",
+                x: 200,
+                y: 100
+            },
+
+            {
+                type: "basic",
+                x: 400,
+                y: 100
+            },
+
+            {
+                type: "turret",
+                x: 700,
+                y: 120
+            }
+        ]
+    },
+
+    2: {
+
+        enemies: [
+
+            {
+                type: "fast",
+                x: 300,
+                y: 80
+            },
+
+            {
+                type: "fast",
+                x: 600,
+                y: 120
+            }
+        ]
+    }
+};
