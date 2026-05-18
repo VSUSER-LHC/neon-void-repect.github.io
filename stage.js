de2abb9db5d448feeccaa5ff55c3b56e7c61a4bb
VSUSER-LHC <hiroki.k.2026@gmail.com>
2026-05-18 13:29:02 +0900

    Refactor STAGES to use 'basic' enemy type

--- stage.js
+++ stage.js
@@ -1,9 +1,9 @@
-const STAGES = {
-    1: {
-        enemies: [
-            { type: "normal", x: 200, y: 100 },
-            { type: "normal", x: 400, y: 100 },
-            { type: "fast", x: 600, y: 100 }
-        ]
-    }
+export const STAGES = {
+  1: {
+    enemies: [
+      { type: "basic", x: 200, y: 100 },
+      { type: "basic", x: 500, y: 150 },
+      { type: "basic", x: 800, y: 120 }
+    ]
+  }
 };
